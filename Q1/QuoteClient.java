05f1c1c6c3bacce1904980cd9416544b09336d64
Alex M <33159385+AlexM5150@users.noreply.github.com>
2022-02-23 15:37:05 -0800

    Update QuoteClient.java
    added date client example to be changed

--- Q1/QuoteClient.java
+++ Q1/QuoteClient.java
@@ -10,5 +10,22 @@ import java.io.*;
 
 public class QuoteClient
 {
-	//Your code is here
+	public static void main(String[] args) {
+		try {
+			//make connection to socket
+			Socket sock = new Socket("127.0.0.1",6017);
+			InputStream in = sock.getInputStream();
+			BufferedReader br = new BufferedReader(new InputStreamReader(in));
+			// read the date from the socket (need to change to get quote)
+			String line;
+			while ((line = br.readLine()) != null){
+				System.out.println(line);
+			}
+			// close the socket connection
+			sock.close();
+		}
+		catch (IOException e) {
+		System.err.println(e);
+		}
+	}
 }
