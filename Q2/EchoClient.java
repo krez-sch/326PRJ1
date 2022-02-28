2a9f09a664a1bc16eb5fce0c73df0ef7edef96ec
krez-sch <72816580+krez-sch@users.noreply.github.com>
2022-02-27 16:02:51 -0800

    Add files via upload
    Added input functionality to client.
    Added echo functionality to server.
    Added client/server text swap to server.

--- Q2/EchoClient.java
+++ Q2/EchoClient.java
@@ -8,5 +8,41 @@ import java.io.*;
 
 public class EchoClient
 {
-	//Your code is here
+    public static void main(String[] args) {
+        String lineUser, lineServer;
+        boolean stayRunning = true;
+        
+        try {
+            // Console application for reading user input
+            Console console = System.console();
+            
+            // Read and send chats while the client is active
+            while (stayRunning) {
+                //make connection to socket
+                Socket sock = new Socket("127.0.0.1", 6017);
+                
+                // Get input from the server
+                InputStream in = sock.getInputStream();
+                BufferedReader br = new BufferedReader(new InputStreamReader(in));
+                
+                // Send output to the server
+                PrintWriter pout = new PrintWriter(sock.getOutputStream(), true);
+                
+                // Read the chat from the user and send it to the server
+                lineUser = console.readLine("\nEnter a line: ");
+                pout.println(lineUser);
+                
+                // read the chat from the server 
+                while ((lineServer = br.readLine()) != null){
+                    System.out.println(lineServer);
+                }
+                
+                // close the socket connection
+                sock.close();
+            }
+        }
+        catch (IOException e) {
+            System.err.println(e);
+        }
+    }
 }
