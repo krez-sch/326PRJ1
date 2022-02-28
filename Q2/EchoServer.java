b629291dbf2e585bab72967fca5195141879e177
krez-sch <72816580+krez-sch@users.noreply.github.com>
2022-02-27 16:05:59 -0800

    Add files via upload

--- Q2/EchoServer.java
+++ Q2/EchoServer.java
@@ -1,13 +1,52 @@
 
 /**
- * An echo server listening on port 6007. This server reads from the client
- * and echoes back the result. 
- */
+* An echo server listening on port 6007. This server reads from the client
+* and echoes back the result.
+*/
 
 import java.net.*;
 import java.io.*;
 
-public class  EchoServer
+public class EchoServer
 {
- //Your code is here
+    public static void main(String[] args) {
+        boolean stayRunning = true;
+    	String line;
+        
+    	try {
+            ServerSocket sock = new ServerSocket(6017);
+        	
+            System.out.println("Listening for connections on port 6017...");
+            
+            while (stayRunning) {
+            	// listen for connections
+                Socket client = sock.accept();
+                
+                // Get input from the client
+                InputStream in = client.getInputStream();
+                BufferedReader br = new BufferedReader(new InputStreamReader(in));
+                
+                // Read output from the client
+                PrintWriter pout = new PrintWriter(client.getOutputStream(), true);
+                
+            	// read the chat from the client 
+                while ((line = br.readLine()) != null){
+                	System.out.println("Received: " + line);
+                	line = line.replaceAll("client", "!temp!").replaceAll("Client", "!Temp!");
+                	line = line.replaceAll("server", "client").replaceAll("Server", "Client");
+                	line = line.replaceAll("!temp!", "server").replaceAll("!Temp!", "Server");
+                	System.out.println("Sent: " + line);
+                    pout.println(line);
+                }
+                
+                // close the socket and resume listening for connections
+                client.close();
+            }
+            
+            sock.close();
+        }
+        catch (IOException e) {
+            System.err.println(e);
+        }
+    }
 }
